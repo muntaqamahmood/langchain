8e1f40c15fee0e2ac8771e5e155c736b5d9465d5
Andrew Qian <andrewqian2001@gmail.com>
2023-11-30 18:00:40 -0500

    fix lint issue

--- libs/langchain/langchain/agents/agent_toolkits/__init__.py
+++ libs/langchain/langchain/agents/agent_toolkits/__init__.py
@@ -14,7 +14,7 @@ whether permissions of the given toolkit are appropriate for the application.
 See [Security](https://python.langchain.com/docs/security) for more information.
 """
 from pathlib import Path
-from typing import Any, List
+from typing import Any
 
 from langchain_core._api.path import as_import_path
 
